16e171600f9184800be843349bc06d5f1a73dda9
goofw <59505666+goofw@users.noreply.github.com>
2024-05-19 23:18:51 +0800

    rebase

--- main.go
+++ main.go
@@ -60,10 +60,10 @@ func get(downloadURL *string) ([]byte, error) {
 
 func download(release *github.RepositoryRelease) ([]byte, error) {
 	geositeAsset := common.Find(release.Assets, func(it *github.ReleaseAsset) bool {
-		return *it.Name == "dlc.dat"
+		return *it.Name == "geosite.dat"
 	})
 	geositeChecksumAsset := common.Find(release.Assets, func(it *github.ReleaseAsset) bool {
-		return *it.Name == "dlc.dat.sha256sum"
+		return *it.Name == "geosite.dat.sha256sum"
 	})
 	if geositeAsset == nil {
 		return nil, E.New("geosite asset not found in upstream release ", release.Name)
@@ -378,14 +378,16 @@ func release(source string, destination string, output string, cnOutput string, 
 	if err != nil {
 		return err
 	}
-	setActionOutput("tag", *sourceRelease.Name)
+	// setActionOutput("tag", *sourceRelease.Name)
+	tagName := *sourceRelease.Name
+	setActionOutput("tag", tagName[12:])
 	return nil
 }
 
 func main() {
 	err := release(
-		"v2fly/domain-list-community",
-		"sagernet/sing-geosite",
+		"Loyalsoldier/v2ray-rules-dat",
+		"goofw/sing-geosite",
 		"geosite.db",
 		"geosite-cn.db",
 		"rule-set",
